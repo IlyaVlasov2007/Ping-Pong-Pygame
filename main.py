3b5bd83b040ad9bb2005722e67bc22fae2418180
IlyaVlasov2007 <vlad_zavyalov_06@vk.com>
2025-05-15 15:15:13 +0500

    Createed Main Loop

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+root = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Ping Pong')
+
+FPS = 60
+
+while True:
+
+    for e in pygame.event.get():
+
+        if e.type == pygame.QUIT:
+            sys.exit(0)
+
+    pygame.display.update()
+    pygame.time.Clock().tick(FPS)
