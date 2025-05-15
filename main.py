442e4d3702376844bafe95727c779af1ebb5b0a1
IlyaVlasov2007 <vlad_zavyalov_06@vk.com>
2025-05-15 16:25:56 +0500

    Create Ball logic | Finished Main Game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame.constants
 
 from Player import *
+from Ball import *
 
 import sys
 
@@ -21,6 +22,9 @@ player_right = Player(x=WIDTH-player_width-10, y=HEIGHT//2-player_height, speed=
                       texture="Assets/Textures/player_right.png",
                       pressed_btn=(pygame.constants.K_UP, pygame.constants.K_DOWN))
 
+ball = Ball(x=WIDTH//2, y=100, width=40, height=40, speed=3,
+            texture="Assets/Textures/ball.png",)
+
 FPS = 60
 
 while True:
@@ -28,14 +32,22 @@ while True:
 
     player_left.draw(root)
     player_right.draw(root)
+    ball.draw(root)
 
     for e in pygame.event.get():
 
         if e.type == pygame.QUIT:
             sys.exit(0)
 
+    if pygame.sprite.collide_rect(ball ,player_left) or pygame.sprite.collide_rect(ball ,player_right):
+        ball.direction_x *= -1
+
+    if ball.rect.top <= 0 or ball.rect.bottom >= HEIGHT:
+        ball.direction_y *= -1
+
     player_left.update()
     player_right.update()
+    ball.update()
 
     pygame.display.update()
     pygame.time.Clock().tick(FPS)
